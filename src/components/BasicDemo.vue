d626731a837f5b41f729deb8f831874d122cf909
JackLiR8 <jiakeli1024@gmail.com>
2019-11-26 20:12:54 +0800

    :construction: In Process: user watch()

--- src/components/BasicDemo.vue
+++ src/components/BasicDemo.vue
@@ -4,17 +4,23 @@
   <button @click="increment">
     Count is: {{state.count}}, double is: {{state.double}}
   </button>
+  <h4>watch</h4>
+  <div class="watch-display"></div>
   <hr>
 </div>
 </template>
 
 <script>
-import {reactive, computed} from '@vue/composition-api';
+import {reactive, computed, watch} from '@vue/composition-api';
 export default {
   setup() {
     const state = reactive({
       count: 0,
       double: computed(() => state.count * 2)
+    })
+
+    watch(() => {
+      document.querySelector('.watch-display').innerHTML = `count is ${state.count}`
     })
 
     function increment() {
