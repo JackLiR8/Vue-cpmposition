1597f48ef02f877448c7353429a2fe157ef14913
JackLiR8 <jiakeli1024@gmail.com>
2019-12-01 00:01:45 +0800

    :sparkles: New Features: ref vs reactive, toRefs()

--- src/components/MousePosition.vue
+++ src/components/MousePosition.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <h2>logic reuse</h2>
+    <h2>logic reuse & ref vs reactive</h2>
+    <h4>use ref()</h4>
     <p>the mouse is at position x:{{x}}, y:{{y}}</p> 
+    <h4>use reactive()</h4>
+    <p>mouse position: ({{xp}},{{yp}})</p>
   </div>
 </template>
 
 <script>
-import {useMousePosition} from '@/utils/mouse'
+import {useMousePosition, useMousePositionReactive} from '@/utils/mouse'
 export default {
   setup() {
-    const { x, y } = useMousePosition()
+    const { x, y } = useMousePosition();
+    const { x: xp, y: yp} = useMousePositionReactive();
 
-    return { x, y }
+    return { x, y, xp, yp }
   }
 }
 </script>
--- src/utils/mouse.js
+++ src/utils/mouse.js
@@ -1,7 +1,7 @@
 /**
  * @file mouse position logic reuse
  */
-import {ref, onMounted, onUnmounted} from '@vue/composition-api'
+import {ref, reactive, onMounted, onUnmounted, toRefs} from '@vue/composition-api'
 
 export function useMousePosition() {
   const x = ref(0);
@@ -21,4 +21,32 @@ export function useMousePosition() {
   })
 
   return { x, y }
+}
+
+/**
+ * reactive 替换 ref
+ * reactive 产生的对象不能解构（destructured）和扩展(spread), 否则对象里的原始数据类型会失去响应性
+ * 可以使用toRefs() 把对象里的原始数据类型转为ref, 这样即使在结构和扩展之后，原始数据类型的属性依然具有响应性
+ */
+export function useMousePositionReactive() {
+  const pos = reactive({
+    x: 0,
+    y: 0
+  })
+
+  function update(e) {
+    pos.x = e.pageX
+    pos.y = e.pageY
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', update)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', update)
+  })
+
+  // 此处若不使用toRefs包裹，解构/扩展后获得的基本数据类型数据会失去响应性
+  return toRefs(pos);
 }
