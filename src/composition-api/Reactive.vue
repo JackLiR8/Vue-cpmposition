fe9b43d33d97c50b98d7437c72eb38ad8b64f17b
JackLiR8 <jiakeli1024@gmail.com>
2019-12-01 23:55:48 +0800

    :sparkles: New Features: api-reactive

---
+++ src/composition-api/Reactive.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>reactive</h3>
+    <p>count {{state.count}}</p>
+  </div>
+</template>
+
+<script>
+import {reactive, onMounted} from '@vue/composition-api'
+export default {
+  setup() {
+    /* reactive 接受一个对象，返回一个proxy对象。等于2.x 里的Vue.observable() */
+    const state = reactive({count: 0});
+
+    onMounted(() => {
+      const timer = setInterval(() => {
+        state.count++;
+      }, 1000);
+
+      setTimeout(() => {
+        clearTimeout(timer);
+      }, 9001);
+    })
+
+    return { state };
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/ApiPractice.vue
+++ src/views/ApiPractice.vue
@@ -3,15 +3,18 @@
     <hr>
     <h2>composition-api</h2>
     <api-setup :position-x="x"/>
+    <reactive />
   </div>
 </template>
 
 <script>
 import ApiSetup from '@/composition-api/Setup'
+import Reactive from '@/composition-api/Reactive'
 import {useMousePosition} from '@/utils/mouse'
 export default {
   components: {
-    ApiSetup
+    ApiSetup,
+    Reactive
   },
   setup() {
     const {x} = useMousePosition();
