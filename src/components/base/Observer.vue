c8db4ad24db7e1ea451c1338c5ea63ce79770eab
JackLiR8 <jiakeli1024@gmail.com>
2019-12-21 15:06:46 +0800

    :construction: In Process: 无限滚动-添加Observer.vue

---
+++ src/components/base/Observer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="observer" ref="root"></div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from '@vue/composition-api'
+export default {
+  setup(props) {
+    const root = ref(null)
+    let observer = ref(null)
+    onMounted(() => {
+      observer = new IntersectionObserver(([entry]) => {
+        if (entry && entry.isIntersecting) {
+          console.log('intersecting')
+        }
+      })
+
+      observer.observe(root.value)
+    })
+
+    onUnmounted(() => {
+      observer.disconnect()
+    })
+
+    return {
+      root
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
