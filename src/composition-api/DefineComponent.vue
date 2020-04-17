3b0f5cc21eb8dc4b63c9b75b075a36d51ded1a42
JackLiR8 <jiakeli1024@gmail.com>
2020-04-17 14:03:38 +0800

    :truck: 重命名：createComponent.vue -> defineComponent.vue

---
+++ src/composition-api/DefineComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>defineComponent</h3>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  props: {
+    foo: String
+  },
+  
+  setup(props) {
+    console.log(props.foo)
+  }
+})
+
+/* 
+  如果组件里只有setup而没有其他选项，可以直接把setup传给defineComponent 
+
+    export default defineComponent((props: { foo: String} ) => {
+      // ...
+    })
+*/
+</script>
